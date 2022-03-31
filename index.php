ac53e970de3088fb85b7dd27a41ca5d08c837970
Daniil <gosthalk@mail.ru>
2022-03-31 22:07:00 +0300

    add news

--- index.php
+++ index.php
@@ -22,21 +22,35 @@ if ($data->type == 'message_new') {
 }
 
 if ($data->type == 'message_new') {
-    if(mb_strtolower($message) === 'спиздани'){
+    if(mb_strtolower($message) === 'спиздани') {
         $vk->sendMessage($peer_id, "Я ебучий автобот");
     }
-    if(mb_strtolower($message) === 'стикер'){
+    if(mb_strtolower($message) === 'стикер') {
         $stickersArray = require_once('src/stickers.php');
         $stickerPackId = random_int(1,6);
         $stickerId = random_int($stickersArray[$stickerPackId][0],$stickersArray[$stickerPackId][1]);
         $vk->sendMessageWithSticker($peer_id, $stickerId);
     }
-    if(mb_strtolower($message) === 'мем'){
+    if(mb_strtolower($message) === 'мем') {
         $owner_id = '-150550417';
         $posts = json_decode($vk->getGroupWallPosts($owner_id, 30), true);
         $ids = $util->getPostsIds($posts);
         $random_post_int = random_int(0, count($ids));
         $vk->sendMessage($peer_id, 'Держи', 'wall' . $owner_id . '_' . $ids[$random_post_int]);
+    }
+    if(preg_match('/(бот_новости_)+[а-яё]{2,}+$/', mb_strtolower($message))) {
+        $news_word = explode('_', mb_strtolower($message))[2];
+        $news = json_decode($util->curlGetRequest('https://mediametrics.ru/satellites/api/search/',
+        [
+            'ac' => 'search',
+            'q' => $news_word,
+            'p' => 0,
+            'c' => 'ru',
+            'callback' => 'JSONP'
+        ]), true);
+
+        $newsString = $util->transformNews($news);
+        $vk->sendMessage($peer_id, $newsString);
     }
 }
 
--- src/Utility.php
+++ src/Utility.php
@@ -14,4 +14,26 @@ class Utility
 
         return $ids;
     }
+
+    public function curlGetRequest($url, $params = [])
+    {
+        $ch = curl_init($url . http_build_query($params));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        $data = curl_exec($ch);
+        curl_close($ch);
+
+        return $data;
+    }
+
+    public function transformNews($news)
+    {
+        $str = 'Новости : ' . PHP_EOL;
+        foreach ($news as $n) {
+            $str .= date('H:i d-m-Y', $n['timestamp']) . ' - ' . $n['title'] . PHP_EOL;
+        }
+
+        return $str;
+    }
 }
