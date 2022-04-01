ef7e21c6435ed0d90ac9c22f443680f9d7ccb0d1
gosthalk <gosthalk@mail.ru>
2022-04-02 00:22:54 +0300

    fix time

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use App\Utility;
 
 require __DIR__ . '/vendor/autoload.php';
 
-date_default_timezone_set('Europe/London');
+date_default_timezone_set('Europe/Moscow');
 
 //$conf = require('config/config.php');
 //$vk = new VkApiGateway($conf['user_token'], $conf['group_token'], '5.131');
