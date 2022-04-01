8a4f42f8d8211ddbd3271d39efcff36251f6122e
gosthalk <gosthalk@mail.ru>
2022-04-02 00:15:24 +0300

    fix memeanswer

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if ($data->type == 'confirmation') {
     exit(getenv('CONFIRMATION_TOKEN'));
 }
 
-//$vk->SendOK();
+$vk->SendOK();
 
 if ($data->type == 'message_new') {
     $from_id = $data->object->message->from_id;
@@ -36,13 +36,11 @@ if ($data->type == 'message_new') {
     }
     if(mb_strtolower($message) === 'мем') {
         $owner_idArray = require_once('src/meme_groups.php');
-        var_dump($owner_idArray);
         $owner_id = random_int(1,6);
-        var_dump($owner_idArray[$owner_id]);
         $posts = json_decode($vk->getGroupWallPosts($owner_idArray[$owner_id], 20), true);
         $ids = $util->getPostsIds($posts);
         $random_post_int = random_int(0, count($ids));
-        $vk->sendMessage($peer_id, 'Держи', 'wall' . $owner_id . '_' . $ids[$random_post_int]);
+        $vk->sendMessage($peer_id, 'Держи', 'wall' . $owner_idArray[$owner_id] . '_' . $ids[$random_post_int]);
     }
     if(preg_match('/(бот_новости_)[а-яё]{2,}/', mb_strtolower($message))) {
         $news_word = explode('_', mb_strtolower($message))[2];
