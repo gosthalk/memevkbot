e5fb4057993e81d52d2f8c01326234e3c335fce1
Daniil <gosthalk@mail.ru>
2022-06-19 14:20:25 +0300

    fix

--- index.php
+++ index.php
@@ -91,9 +91,8 @@ if ($data->type == 'message_new') {
         $vk->sendMessage($peer_id, $weather);
         return;
     }
-    if(preg_match('/(бот_поиск_)[а-яёa-z]{2,}/', mb_strtolower($message))) {
+    if(preg_match('/(бот_поиск_)[а-яёa-z+]{2,}/', mb_strtolower($message))) {
         $search_word = explode('_', mb_strtolower($message))[2];
-        $vk->sendMessage($peer_id, mb_strtolower($search_word));
         $vk->sendMessage($peer_id, 'https://www.google.ru/search?q=' . mb_strtolower($search_word));
         return;
     }
