6e8957d0c954f0c9bc5103d395e1a76a898c821f
Daniil <gosthalk@mail.ru>
2022-06-19 16:05:11 +0300

    add new feature

--- index.php
+++ index.php
@@ -103,6 +103,13 @@ if ($data->type == 'message_new') {
         $vk->sendMessage($peer_id, 'https://www.google.ru/search?q=' . mb_strtolower($wiki_search_word));
         return;
     }
+    if(preg_match('/(бот_посчитай_){1,20}/u', mb_strtolower($message))) {
+        $expression = explode('_', mb_strtolower($message))[2];
+        $responseHtml = $util->curlGetRequest('https://api.mathjs.org/v4/?expr=' . $expression);
+        preg_match('/<body>([0-9]+\.[0-9]+)/u',$responseHtml,$result);
+        $vk->sendMessage($peer_id, 'Ответ -> ' . $result[1]);
+        return;
+    }
     if(random_int(1,150) === 33) {
         $message = $util->transfromWordsHuebot($message);
         $vk->sendMessage($peer_id, $message);
