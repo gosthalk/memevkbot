86bf81555f09c5221cc74c19f4fe121cdc54d852
Daniil <gosthalk@mail.ru>
2022-06-19 13:31:40 +0300

    fix search

--- index.php
+++ index.php
@@ -92,7 +92,8 @@ if ($data->type == 'message_new') {
         return;
     }
     if(preg_match('/(бот_поиск_)[а-яё]{2,}/', mb_strtolower($message))) {
-        $vk->sendMessage($peer_id, 'https://g.zeos.in/?q=' . mb_strtolower($message));
+        $word = explode("_", $message);
+        $vk->sendMessage($peer_id, 'https://g.zeos.in/?q=' . mb_strtolower($message[2]));
         return;
     }
     if(random_int(1,150) === 33) {
