d7d1dffb6f4dc86104fb576d4032c2ff74539af4
gosthalk <gosthalk@mail.ru>
2022-06-25 19:55:35 +0300

    debug

--- index.php
+++ index.php
@@ -121,14 +121,23 @@ if ($data->type == 'message_new') {
         //$file_created = $tts->createOpusFileFromText($speech);
         //if($file_created) {
 
+            $upload_link = $util->curlGetRequest('https://api.vk.com/method/docs.getMessagesUploadServer?', [
+                'type' => 'audio_message',
+                'peer_id' => '-212296161',
+                'access_token' => $audio_upload_group_token,
+                'v' => $api_version
+            ]);
+            error_log(gettype($upload_link));
+            error_log(print_r($upload_link));
+
             $upload_link = json_decode($util->curlGetRequest('https://api.vk.com/method/docs.getMessagesUploadServer?', [
                 'type' => 'audio_message',
                 'peer_id' => '-212296161',
                 'access_token' => $audio_upload_group_token,
                 'v' => $api_version
-            ]) , true);
-            error_log(gettype($upload_link));
-            error_log(print_r($upload_link));
+            ]), true);
+            error_log($upload_link[1]);
+
 
             $file_link = json_decode($util->curlPostRequest($upload_link['upload_url'], ['file' => realpath('tmp_file.opus')]), true);
 
