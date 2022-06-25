7d33448568d926edb195bc46fb7050e3df948b50
gosthalk <gosthalk@mail.ru>
2022-06-25 21:17:26 +0300

    del file

--- index.php
+++ index.php
@@ -119,9 +119,9 @@ if ($data->type == 'message_new') {
     if(preg_match('/(бот_скажи_)[а-яёa-z]{2,}/u', mb_strtolower($message))) {
         $speech = explode('_', mb_strtolower($message))[2];
 
-        $file_created = $tts->createOpusFileFromText($speech);
-        sleep('20');
-        if($file_created) {
+        //$file_created = $tts->createOpusFileFromText($speech);
+        //sleep('20');
+        //if($file_created) {
 
             $upload_link = json_decode($util->getAudioMessageUploadLink(), true);
             error_log(print_r($upload_link));
@@ -134,9 +134,9 @@ if ($data->type == 'message_new') {
 
             $vk->sendMessageWithAudio($peer_id, 'doc' . $saved_audio_file['owner_id'] . '_' . $saved_audio_file['id']);
             $tts->deleteTmpFiles();
-        } else {
-            $vk->sendMessage($peer_id, 'Не скажу');
-        }
+//        } else {
+//            $vk->sendMessage($peer_id, 'Не скажу');
+//        }
 
         return;
     }
